0a1d760d48893da16a7e9664dd9c21691e7bc2df
marcosenp <marcospne2103@gmail.com>
2025-04-08 16:34:37 -0300

    Adiciona solução HelloWorld com testes unitários

---
+++ MinhaApp.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using MinhaApp;
+
+namespace MinhaApp.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Somar_DeveRetornarSomaCorreta()
+        {
+            int resultado = Calculadora.Somar(2, 3);
+            Assert.Equal(5, resultado);
+        }
+    }
+}
---
+++ MinhaApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MinhaApp
+{
+    public class Calculadora
+    {
+        public static int Somar(int a, int b) => a + b;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, testes!");
+        }
+    }
+}
